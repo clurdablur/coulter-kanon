a6ab0baf7a262c0fc7e8ae88614557a9cbf1fb41
clurdablur <coucc15@gmail.com>
2017-10-15 16:14:02 -0500

    now using node express, updated registry

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-$(document).ready(function () {
+var express = require("express");
+var app = express();
 
-    //Navbar resize
-    $(window).scroll(function() {
-      if ($(document).scrollTop() > 325) {
-        $('nav').removeClass('hidden');
-      }
-      else {
-      	$('nav').addClass('hidden');
-      }
-    });
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
-  });
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/rsvp', function(req, res){
+  res.render('rsvp');
+});
+
+app.get('/photos', function(req, res){
+  res.render('photos');
+});
+
+app.get('/events', function(req, res){
+  res.render('events');
+});
+
+app.get('/wedding-party', function(req, res){
+  res.render('wedding-party');
+});
+
+app.get('/travel', function(req, res){
+  res.render('travel');
+});
+
+app.get('/registry', function(req, res){
+  res.render('registry');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
