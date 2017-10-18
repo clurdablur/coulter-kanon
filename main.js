17115d17c1fbdf1b8958013d9a650a7f21f19b13
clurdablur <coucc15@gmail.com>
2017-10-18 11:11:11 -0500

    changed listen port

--- main.js
+++ main.js
@@ -118,6 +118,6 @@ function isLoggedIn(req, res, next){
     res.redirect('/login');
 }
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
